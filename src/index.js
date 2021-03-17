ea6d1902da0bdb5f0e1cd5b028cafabd5e125020
ry <renyi2009@qq.com>
2021-03-17 17:53:11 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import x from './x.js'
 import png from './assets/1.png'
 
-console.log('--hahaha--')
+console.log('--yoyoyo--')
 console.log(png)
 const div = document.getElementById('app')
 /*div.innerHTML =  `
