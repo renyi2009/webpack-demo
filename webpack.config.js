0cb46b82af9089e71816624b96e547079585eb2a
ry <renyi2009@qq.com>
2021-03-17 11:03:34 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: 'index.[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'yoyo',
+    template: "src/assets/index.html"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["css-loader"],
+      },
+    ],
   },
 };
+
