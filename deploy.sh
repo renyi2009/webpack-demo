373167facf664dd590f2b0803a453d0553b15dc1
ry <renyi2009@qq.com>
2021-03-17 17:47:34 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
 rm -rf dist &&
-ga . && gc -m 'update' && gp &&
-git checkout -
+ga . && gc -m 'update' && gp && git checkout -
