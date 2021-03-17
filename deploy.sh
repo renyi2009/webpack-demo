d7a31f107b730ec56145b6812f08f1f072ad0522
ry <renyi2009@qq.com>
2021-03-17 17:52:12 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,1 @@
-yarn build &&
-git checkout gh-pages &&
-rm -rf *.html *.js *.css *.png &&
-mv dist/* ./ &&
-rm -rf dist &&
-ga . && gc -m 'update' && gp && git checkout -
+yarn build && git checkout gh-pages && rm -rf *.html *.js *.css *.png && mv dist/* ./ && rm -rf dist && ga . && gc -m 'update' && gp && git checkout -
